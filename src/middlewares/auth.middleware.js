c17d453322d2d3716af725192bac4b4ba23af768
Yashraj Patil <yashrajspatil808@gmail.com>
2026-06-15 20:14:09 +0530

    added refresh token functionality and improved JWT verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError"
 import { asyncHandler } from "../utils/asyncHandler"
-
+import jwt from "jsonwebtoken"
 
 
 export const verifyJWT = asyncHandler( async (req, res, next) => {
@@ -9,4 +9,14 @@ export const verifyJWT = asyncHandler( async (req, res, next) => {
     if(!token) {
         throw new ApiError(401, "Unauthorised request")
     }
+
+    const decodedtoken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+    if(!user){
+        
+        throw new ApiError(401, "Invalid Access Token")
+    }
+
 })
